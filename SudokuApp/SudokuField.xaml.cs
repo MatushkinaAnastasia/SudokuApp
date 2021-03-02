01092f6a433970c3190120673c9aa28316c6defc
MatushkinaAnastasia <46565861+MatushkinaAnastasia@users.noreply.github.com>
2021-03-02 14:51:26 +0500

    Add filling sudoku field

--- SudokuApp/SudokuField.xaml.cs
+++ SudokuApp/SudokuField.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +21,27 @@ namespace SudokuApp
 	/// </summary>
 	public partial class SudokuField : Window
 	{
+		private readonly int _size;
+		private readonly int[,] _data;
 		public SudokuField()
 		{
 			InitializeComponent();
+
+			_size = 9;
+			_data = GetDataFromFile(@"C:\Users\nasya\source\repos\SudokuApp\data.txt");
+			for (int i = 0; i < _size; i++)
+			{
+				for (int j = 0; j < _size; j++)
+				{
+					Console.Write(_data[i, j]);
+				}
+				Console.WriteLine();
+			}
 			CreateGrid();
 		}
 
 		private void CreateGrid()
 		{
-			var r = new Random();
-
 			for (int i = 0; i < 3; i++)
 			{
 				grid.RowDefinitions.Add(new RowDefinition());
@@ -58,6 +70,7 @@ namespace SudokuApp
 						little_grid.ColumnDefinitions.Add(new ColumnDefinition());
 					}
 
+					
 					for (int a = 0; a < 3; a++)
 					{
 						for (int b = 0; b < 3; b++)
@@ -68,17 +81,31 @@ namespace SudokuApp
 								BorderThickness = new Thickness(0.6),
 							};
 
+							var value = _data[i * 3 + a, j * 3 + b].ToString();
 
 							var text_box = new TextBox
 							{
-								Text = r.Next(1, 10).ToString(),
 								TextAlignment = TextAlignment.Center,
 								HorizontalAlignment = HorizontalAlignment.Center,
 								VerticalAlignment = VerticalAlignment.Center,
 								FontSize = 24,
 								BorderThickness = new Thickness(0),
 								MaxLength = 1,
+								Width = 40,
 							};
+
+							if (value == "0")
+							{
+								text_box.Text = "";
+							}
+							else
+							{
+								text_box.Text = value;
+								text_box.IsEnabled = false;
+								text_box.Foreground = Brushes.Black;
+								text_box.FontWeight = FontWeights.UltraBold;
+							}
+
 
 							little_grid.Children.Add(little_border);
 							Grid.SetRow(little_border, a);
@@ -90,10 +117,31 @@ namespace SudokuApp
 						}
 					}
 
-
 				}
 			}
 
+		}
+
+		private int[,] GetDataFromFile(string path)
+		{
+			var array = new int[_size, _size];
+			using (var reader = new StreamReader(path))
+			{
+				for (int i = 0; i < _size; i++)
+				{
+					var line = reader.ReadLine();
+					for (int j = 0; j < _size; j++)
+					{
+						var symbol = line[j];
+						if (symbol != '*')
+						{
+							array[i, j] = int.Parse(symbol.ToString());
+						}
+					}
+				}
+			}
+
+			return array;
 		}
 	}
 }
